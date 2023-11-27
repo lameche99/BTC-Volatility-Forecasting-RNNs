afa24c04e36426e5a4dec0d44b99cc228c9853b9
lameche99 <85850159+lameche99@users.noreply.github.com>
2023-11-27 02:01:15 -0500

    tests

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from keras.layers import LSTM, GRU, Dense, Reshape
 from keras.models import Sequential
@@ -11,23 +12,26 @@ import optuna
 from optuna.trial import TrialState
 from optuna.samplers import TPESampler
 
-engine = sqlite3.connect('./btc-data.db')
-btc = pd.read_sql('SELECT * FROM BTCUSD', engine)
-logRet = btc.logRet.values[-13920:]
-n = len(logRet)
-train_size = 9216
-val_size = 6912
-dt = 96
-train_x = logRet[:val_size].reshape(int(0.375*dt), dt, 2)
-val_x = logRet[val_size:train_size].reshape(int(0.125*dt), dt, 2)
-full_train_x = logRet[:train_size].reshape((dt//2),dt,2)
-test_x = logRet[train_size:(n-dt)].reshape(int(0.25*dt),dt,)
-train_y = logRet[dt:(val_size+dt)].reshape(int(0.75*dt), dt, 1)
-val_y = logRet[(val_size+dt):(train_size+dt)].reshape(int(0.25*dt), dt, 1)
-full_train_y = logRet[dt:(train_size+dt)].reshape(dt,dt,1)
-test_y = logRet[(train_size+dt):n].reshape(int(0.5*dt),dt,1)
-min_ = np.min(full_train_x)
-scale_ = np.max(full_train_x) - min_
+# engine = sqlite3.connect('./btc-data.db')
+# btc = pd.read_sql('SELECT * FROM BTCUSD', engine)
+btc = pd.read_csv('./sentiment.csv')
+scaler = MinMaxScaler()
+scaler.fit(btc[['logRet', 'sentiment_score']].values)
+n = len(btc)
+dt = 44
+scaled = scaler.transform(btc[['logRet', 'sentiment_score']].values)[:n-dt] # X matrix
+logRet = scaler.transform(btc[['logRet', 'sentiment_score']].values)[dt:, 0] # y values
+min_ = scaler.min_ # min scalers
+scale_ = scaler.scale_ # scales
+X = scaled.reshape(29, 22, 2)
+Y = logRet.reshape(29, 22, 1)
+
+train_size = int(0.6 * len(X))
+val_size = int(0.8 * len(X))
+train_x, train_y = X[:train_size], Y[:train_size]
+val_x, val_y = X[train_size:val_size], Y[train_size:val_size]
+test_x, test_y = X[val_size:], Y[val_size:]
+full_train_x, full_train_y = X[:val_size], Y[:val_size]
 
 def squared_epsilon_insensitive_loss(epsilon: float = 0.025):
     """
@@ -63,7 +67,7 @@ def invTransform(sig: np.array):
     X = tf.reshape(sig[0,:,0],[sig.shape[1],1])
     X -= scalermin_
     X /= scalerscale_
-    return tf.reshape(X, [1,96,1])
+    return tf.reshape(X, [1,44,1])
 
 def calculateRV(signal: np.array):
     """
@@ -103,8 +107,8 @@ def create_model(trial, mode: int = 1):
     else:
         model.add(GRU(units=rec_units, dropout=dropout_units))
     
-    model.add(Dense(units=96))
-    model.add(Reshape((96,1)))
+    model.add(Dense(units=22))
+    model.add(Reshape((22,1)))
 
     # Compile model.
     model.compile(
@@ -141,7 +145,7 @@ def train_model(trial, train_x: np.array = train_x,
     history = model.fit(
         x=train_x,
         y=train_y,
-        epochs=30,
+        epochs=15,
         validation_data=(valid_x, valid_y),
         callbacks=callbacks,
         shuffle=False,
@@ -155,10 +159,10 @@ def create_study():
         direction="minimize", sampler=TPESampler(multivariate=True), storage=f"sqlite:///test_rnn.db",
         load_if_exists=True, pruner=optuna.pruners.NopPruner()
     )
-    study.optimize(train_model, n_trials=250, catch=(Exception,))
+    study.optimize(train_model, n_trials=25, catch=(Exception,))
     return study
 
-def show_result(study):
+def get_optimized_parameters(study):
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
@@ -173,9 +177,94 @@ def show_result(study):
 
     print("  Value: ", trial.value)
 
-    print("  Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+    return dict(trial.params.items())
+
+def test_model(rec_units, dropout_units,
+                learning_rate, weight_decay,
+                epsilon,
+                train_x: np.array = train_x,
+                train_y: np.array = train_y,
+                valid_x: np.array = val_x,
+                valid_y: np.array = val_y,
+                mode: int = 1):
+    model = Sequential()
+    if mode == 1:
+        model.add(LSTM(units=rec_units, dropout=dropout_units))
+    else:
+        model.add(GRU(units=rec_units, dropout=dropout_units))
     
-    best_test_result = train_model(trial, train_x=full_train_x, train_y=full_train_y, valid_x=test_x, valid_y=test_y)
-    print(f"Best result in test set: {best_test_result}")
+    model.add(Dense(units=22))
+    model.add(Reshape((22,1)))
+
+    # Compile model.
+    model.compile(
+        optimizer=AdamW(
+            learning_rate=learning_rate,
+            weight_decay=weight_decay
+        ),
+        loss=squared_epsilon_insensitive_loss(epsilon=epsilon),
+        metrics=mape
+    )
+    model.fit(
+        x=train_x,
+        y=train_y,
+        epochs=15,
+        validation_data=(valid_x, valid_y),
+        shuffle=False,
+        batch_size=1
+    )
+
+    y_pred = model.predict(
+        x=test_x
+    )
+    return y_pred
+
+def invPred(sig):
+    """Inverse transform scaled signals
+    Parameters
+    ----------
+    sig : np.ndarray; (_days, _steps, _channels)
+        Signals wrt given dimensions
+
+    df_scale : DataFrame
+        Respective scale dataframe with columns mins and scales necessary
+        for inverse_transform function.
+
+    Returns
+    -------
+    to_ret : np.ndarray; (_days, _steps, _channels)
+        Converted back to Log-Return Scales
+    """
+    to_ret = np.zeros((sig.shape[0], sig.shape[1], sig.shape[2]))
+    print(np.shape(to_ret))
+    for i in range(sig.shape[0]):
+        for j in range(sig.shape[2]):
+            scaler = MinMaxScaler()
+            scaler.min_, scaler.scale_ = min_[j], scale_[j]
+            to_ret[i,:,j] = scaler.inverse_transform(sig[i,:,j].reshape(sig.shape[1],1)).reshape(sig.shape[1],)
+
+    return to_ret
+
+
+def calculate_RV(signal):
+    """Calcualte RV from log returns
+    Parameters
+    ----------
+    signal : np.ndarray
+        Input array with first dimension as days and last dimension as log-returns
+    Returns
+    -------
+    np.ndarray
+        [description]
+    """
+    return 100 * np.sqrt(np.square(signal).sum(axis=-1))
+
+
+def evaluate_forecast(forecast, actual):
+    mape = np.mean(np.abs(forecast - actual) / np.abs(actual))
+    me = np.mean(forecast - actual)
+    mae = np.mean(np.abs(forecast - actual))
+    mpe = np.mean((forecast - actual) / actual)
+    rmse = np.mean((forecast - actual) ** 2) ** 0.5
+    msle = np.mean(np.square(np.log(1 + forecast) - np.log(1 + actual)))
+    return {"mape": mape, "me": me, "mae": mae, "mpe": mpe, "rmse": rmse, "msle": msle}
